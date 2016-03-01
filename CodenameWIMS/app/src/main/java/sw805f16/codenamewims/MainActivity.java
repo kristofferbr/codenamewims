9e2357dc09914517579f673766cefaee27afa077
Kristoffer Brodersen <kristofferbrodersen@gmail.com>
2016-03-01 13:39:44 +0100

    Clean setup, on wrong path

---
+++ CodenameWIMS/app/src/main/java/sw805f16/codenamewims/MainActivity.java
@@ -0,0 +1,13 @@
+package sw805f16.codenamewims;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
