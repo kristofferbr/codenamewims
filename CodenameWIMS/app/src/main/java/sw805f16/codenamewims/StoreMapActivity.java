a2502008d6b37b14653e3d660a95b84561122bf7
Kristoffer Brodersen <kristofferbrodersen@gmail.com>
2016-03-10 15:53:40 +0100

    Development of use case Store Map has commenced

--- CodenameWIMS/app/src/main/java/sw805f16/codenamewims/StoreMapActivity.java
+++ CodenameWIMS/app/src/main/java/sw805f16/codenamewims/StoreMapActivity.java
@@ -2,16 +2,28 @@ package sw805f16.codenamewims;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class StoreMapActivity extends AppCompatActivity {
 
+
+    // URL til map /api/store/ID/map
     public boolean isInFront = false;
-    public String store_id;
+    public String store_id = "someID";
+    RequestQueue rqueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_map);
+        rqueue = Volley.newRequestQueue(this);
     }
 
     @Override
@@ -25,4 +37,30 @@ public class StoreMapActivity extends AppCompatActivity {
         super.onPause();
         isInFront = false;
     }
+
+    public void getMapLayout(){
+
+        String url = "http://nielsema.ddns.net:3000/api/store/" + store_id +"/map";
+        StringRequest stringreq = new StringRequest(Request.Method.GET,url,
+                new Response.Listener<String>(){
+
+                    @Override
+                    public void onResponse(String response){
+                        /*Set this response to a textview of sorts*/
+                    }
+                },
+                new Response.ErrorListener(){
+
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        /*Do something with the error*/
+                    }
+                }
+        );
+
+        rqueue.add(stringreq);
+    }
+
+
+
 }
--- CodenameWIMS/app/src/test/java/sw805f16/codenamewims/StoreMapActivityTest.java
+++ CodenameWIMS/app/src/test/java/sw805f16/codenamewims/StoreMapActivityTest.java
@@ -1,5 +1,12 @@
 package sw805f16.codenamewims;
 
+import android.widget.ImageView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
@@ -13,11 +20,16 @@ import org.robolectric.annotation.Config;
 @Config(constants = BuildConfig.class)
 public class StoreMapActivityTest {
 
+    @Test
     public void tests(){
 
         StoreMapActivity storemap = Robolectric.setupActivity(StoreMapActivity.class);
 
-        
+        ImageView image = (ImageView)storemap.findViewById(R.id.storemap);
+
+        storemap.getMapLayout();
+        Assert.assertNotNull(image.getDrawable());
+
 
 
     }
