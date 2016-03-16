18cd616a0da633b1e95496eafd9de89acf6b410f
Rasmus <rjohns12@student.aau.dk>
2016-03-16 16:28:13 +0100

    Some git troubles should be up

--- CodenameWIMS/app/src/main/java/sw805f16/codenamewims/MainActivity.java
+++ CodenameWIMS/app/src/main/java/sw805f16/codenamewims/MainActivity.java
@@ -1,10 +1,12 @@
 package sw805f16.codenamewims;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.opengl.Matrix;
 import android.service.voice.VoiceInteractionSession;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -16,10 +18,38 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.*;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.AbstractSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import info.debatty.java.stringsimilarity.Jaccard;
+import info.debatty.java.stringsimilarity.Levenshtein;
+import info.debatty.java.stringsimilarity.NormalizedLevenshtein;
 
 
 /**
@@ -29,42 +59,121 @@ import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
-    Boolean clicked = false;
-    ScaleGestureDetector Scale;
-    Matrix matrix;
-    float scale = 1;
-    int i = 1;
-    ImageView map;
-
-    /*Test for the test framework*/
-    public int addTwoNumbers(int x, int y)
-    {
-        return x+y;
-    }
-
+    HashMap<String, String> stores = new HashMap<>();
+    ArrayList<String> rankedResults = new ArrayList<>();
+    JSONArray json;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        try {
+//            RequestQueue queue = Volley.newRequestQueue(this);
+//            String url = "http://nielsema.ddns.net/sw8/api/store";
+//
+//            request(queue, url);
+//            extractInformationFromJson(json);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
 
-        /*Instantiate a Request Queue and a Requrst String*/
-        //final RequestQueue rqueue = Volley.newRequestQueue(this);
-        //final String url = "http://nielsema.ddns.net:3000/api/product";
+    }
 
-        Button but = (Button) findViewById(R.id.chooseStore);
+    /**
+     * This method is used by the test classes to evaluate the stores hashmap
+     * @return The stores HashMap of this instance
+     */
+    public HashMap<String, String> getStores() {
+        return stores;
+    }
 
-        but.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),StoreMapActivity.class);
+    /**
+     * This method is used by the test classes to evaluate the ranked search results list
+     * @return The rankedResults ArrayList of this instance
+     */
+    public ArrayList<String> getRankedSearchResults() {
+        return rankedResults;
+    }
 
-                startActivity(intent);
-                /*Start the storemap activity*/
+
+    /**
+     * A method that extracts information from a json array and puts it in a hashmap
+     * @param jsonArray The json array from the server
+     * @throws JSONException Every JSONObject and JSONArray method throws this, therefore This throws it as well
+     */
+    public void extractInformationFromJson(JSONArray jsonArray) {
+        try {
+            JSONObject tmpObject;
+            //Because this method is only called when we have a new JSON array we clear stores
+            stores.clear();
+
+            //Here we loop over the json objects in the json array
+            for (int i = 0; i < jsonArray.length(); i++) {
+                tmpObject = jsonArray.getJSONObject(i);
+                //We extract the storename and the id and place them in a HasMap
+                stores.put(tmpObject.getString("name").toLowerCase(), tmpObject.getString("_id"));
             }
-        });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
+    /**
+     * This method ranks the stores after similarity between the query and the keys in the stores HashMap
+     * @param query The search query
+     */
+    public void rankSearchResults(String query) {
+        String key = "";
+        //We have a local HashMap of storenames and associated rank
+        HashMap<String, Double> ranks = new HashMap<>();
+        //We use the normalized levenshtein similarity between the strings
+        NormalizedLevenshtein similarity = new NormalizedLevenshtein();
+        Iterator iterator;
+
+        //We instantiate the iterator with the stores HasMap iterator
+        iterator = stores.entrySet().iterator();
+        Map.Entry pair;
+
+        //Here we iterate through the stores HashMap
+        while(iterator.hasNext()) {
+            //We extract the pairs
+            pair = (Map.Entry) iterator.next();
+            key = (String) pair.getKey();
+            //We add the key and the similarity score with the query to the ranks HashMap
+            ranks.put(key, similarity.similarity(query, key));
+        }
+
+        double highest;
+
+        //Here we use the global store HashMap instead of the local ranks, because they initially are the same size
+        for (int i = 0; i < stores.size(); i++) {
+            //We re-instantiate the iterator to iterate over ranks HashMap and set the highest value to 0
+            iterator = ranks.entrySet().iterator();
+            highest = 0;
+
+            //Here we iterate over the ranks HashMap
+            while (iterator.hasNext()) {
+                pair = (Map.Entry) iterator.next();
+
+                //If the value in the extracted pair is greater than or equal to the highest value and above a threshold
+                //The string key is set the pair key and the highest value is updated
+                if (((Double) pair.getValue()) >= highest && ((Double) pair.getValue()) > 0.5) {
+                    key = (String) pair.getKey();
+                    highest = (Double) pair.getValue();
+                }
+            }
+            //If the highest value has not been set and there are more than one entry in the ranks HashMap
+            //Then that means there were no meaningful matches for the query
+            if (highest == 0 && ranks.size() > 1) {
+                Toast.makeText(MainActivity.this, "There was no match to the search", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            //When the storename with the highest similarity with query is found it is added to the list
+            rankedResults.add(key);
+            //When we are done with the storename it is removed from the ranks HashMap
+            ranks.remove(key);
+        }
     }
 
 
@@ -73,33 +182,22 @@ public class MainActivity extends AppCompatActivity {
      * @param req The queue to add the request
      * @param url The url to request
      */
-//    public void request(RequestQueue req, String url){
-//
-//        StringRequest stringreq = new StringRequest(Request.Method.GET,url,
-//                new Response.Listener<String>(){
-//
-//                    TextView tex = (TextView) findViewById(R.id.response);
-//
-//                    @Override
-//                    public void onResponse(String response){
-//                        /*Set this response to a textview of sorts*/
-//                        tex.setText(response.substring(0));
-//                    }
-//
-//                },
-//                new Response.ErrorListener(){
-//                    TextView tex = (TextView) findViewById(R.id.response);
-//                    @Override
-//                    public void onErrorResponse(VolleyError error){
-//                        tex.setText("INGEN FORBINDELSE");
-//                        /*Do something with the error*/
-//                    }
-//                }
-//        );
-//
-//        req.add(stringreq);
-//    }
+    private void request(RequestQueue req, String url){
 
+        JsonArrayRequest jsonRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                json = response;
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "Could not retrieve data from the server", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        req.add(jsonRequest);
+    }
 
 
 
--- CodenameWIMS/app/src/test/java/sw805f16/codenamewims/ChoosingaStoreTest.java
+++ CodenameWIMS/app/src/test/java/sw805f16/codenamewims/ChoosingaStoreTest.java
@@ -1,7 +1,15 @@
 package sw805f16.codenamewims;
+import android.test.suitebuilder.annotation.Suppress;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import org.apache.tools.ant.helper.ProjectHelper2;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.Rule;
@@ -11,6 +19,17 @@ import org.junit.runner.*;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowIntent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import imports.StartedMatcher;
+
+import static org.junit.Assert.*;
+import static org.robolectric.Shadows.*;
+import static org.hamcrest.CoreMatchers.*;
 
 /**
  * Created by kbrod on 09/03/2016.
@@ -20,21 +39,54 @@ import org.robolectric.annotation.Config;
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class ChoosingaStoreTest {
+    MainActivity main;
+    SearchView search;
+    JSONArray dummyJson;
+
+    @Before
+    public void setup() {
+        main = Robolectric.setupActivity(MainActivity.class);
+        search = (SearchView) main.findViewById(R.id.search);
+
+        //This is a JSON array, in string format, we want to extract information from
+        String jsonString = "[\n" +
+                "  {\n" +
+                "    \"_id\": \"56e6a28a28c3e3314a6849df\",\n" +
+                "    \"name\": \"Føtex\",\n" +
+                "    \"description\": \"Føtex er sej vi gør mere for dig\",\n" +
+                "    \"map\": \"56e6a32e28c3e3314a6849e3\",\n" +
+                "    \"__v\": 1,\n" +
+                "    \"products\": [\n" +
+                "      \"56e6a9f028c3e3314a6849ea\",\n" +
+                "      \"56e6aa1b28c3e3314a6849eb\"\n" +
+                "    ]\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"_id\": \"56e6a28a28c3e3314a6849e0\",\n" +
+                "    \"name\": \"Netto\",\n" +
+                "    \"description\": \"Netto – Derfor\",\n" +
+                "    \"products\": []\n" +
+                "  }\n" +
+                "]";
+        try {
+            dummyJson = new JSONArray(jsonString);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     // As a user
     // I want to be able to choose the store
     // so that I can retrieve the correct map and item list
-    public void choosing_a_store(){
+    public void choosing_a_store_test(){
         // As a user
         // When I am at the home screen
-
         // I can tap the search bar
-
         // Then I can enter the name of a store
+        search.setQuery("Føtex", true);
 
         // I then retreive a list of candidates to choose from
-
 
         // As a user
         // When I am at SOME PLACE
@@ -47,6 +99,31 @@ public class ChoosingaStoreTest {
 
         // Repeat indtil det er implementeret i alle steder i applikationen
 
+    }
+
+    @Test
+    public void extract_information_from_json_test() {
+        //The string is made into an actual JSONArray and passed to the method
+        main.extractInformationFromJson(dummyJson);
+
+        HashMap<String, String> testMap = main.getStores();
+        //Here we assert whether it has extracted the name correctly and whether it is the right id
+        assertTrue(testMap.containsKey("føtex"));
+        assertTrue(testMap.get("føtex").equals("56e6a28a28c3e3314a6849df"));
+    }
+
+    @Test
+    public void search_result_ranking() {
+        main.extractInformationFromJson(dummyJson);
+        main.rankSearchResults("føtex");
+
+        ArrayList<String> testList = main.getRankedSearchResults();
+        assertThat(testList.get(0), is("føtex"));
+        assertTrue(!testList.contains("netto"));
+
+        main.rankSearchResults("tex");
+        testList = main.getRankedSearchResults();
+        assertThat(testList.get(0), is("føtex"));
     }
 
 }
