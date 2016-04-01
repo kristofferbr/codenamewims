760443805284eaebb81dcd5e14186dadebd90b3b
Patrick Grønhøj <pgronh12@student.aau.dk>
2016-04-01 14:55:58 +0200

    Start screen is in.

---
+++ CodenameWIMS/app/src/main/java/sw805f16/codenamewims/ShoppingActivity.java
@@ -0,0 +1,57 @@
+package sw805f16.codenamewims;
+
+/**
+ * Created by Netray on 01/04/2016.
+ */
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ShoppingActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+
+
+        addShoppingList("Harold");
+
+    }
+
+    public void addShoppingList(String name){
+        List<String> items = new ArrayList<String>();
+
+        items.add("Chicken");
+        items.add("Butter");
+        items.add("Chili Cheese Chops");
+        items.add("Milk");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.shopping_list_note, null, false);
+
+        TextView title = (TextView)layout.findViewById(R.id.shopping_title);
+        title.setText(name);
+
+        TextView titems = (TextView)layout.findViewById(R.id.shopping_items);
+        titems.setText("Butter");
+
+
+        LinearLayout linear = (LinearLayout)findViewById(R.id.shopping_lists);
+        linear.addView(layout);
+    }
+}
---
+++ CodenameWIMS/app/src/main/java/sw805f16/codenamewims/StartActivity.java
@@ -0,0 +1,64 @@
+package sw805f16.codenamewims;
+
+/**
+ * Created by Netray on 01/04/2016.
+ */
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.*;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
+public class StartActivity extends AppCompatActivity {
+
+    Parcelable fragmentState;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen); // When refactoring, change to activity_start_screen layoutet.
+
+        if (getIntent().getParcelableExtra("state") != null) {
+            fragmentState = getIntent().getParcelableExtra("state");
+        }
+
+
+        Button storemapButton = (Button) findViewById(R.id.startStoreBtn);
+        Button shoppingButton = (Button) findViewById(R.id.startShoppingBtn);
+        ImageButton settingsButton = (ImageButton) findViewById(R.id.startSettingsBtn);
+        Button exitButton = (Button) findViewById(R.id.startExitBtn);
+
+
+        shoppingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ShoppingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
