cc8c130c9f221d9287215494ab4dae21a20e5962
Patrick Grønhøj <pgronh12@student.aau.dk>
2016-04-18 09:48:19 +0200

    Added new button to start screen.

--- CodenameWIMS/app/src/main/java/sw805f16/codenamewims/StartActivity.java
+++ CodenameWIMS/app/src/main/java/sw805f16/codenamewims/StartActivity.java
@@ -11,9 +11,12 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class StartActivity extends Activity {
+
+    int storeID;
 
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE); // Removes action bar for start screen.
@@ -24,6 +27,15 @@ public class StartActivity extends Activity {
         Button shoppingButton = (Button) findViewById(R.id.startShoppingBtn);
         ImageButton settingsButton = (ImageButton) findViewById(R.id.startSettingsBtn);
         Button exitButton = (Button) findViewById(R.id.startExitBtn);
+        Button chooseStoreButton = (Button) findViewById(R.id.startChooseStoreBtn);
+
+
+        chooseStoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(StartActivity.this, "REPLACE ME!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         storemapButton.setOnClickListener(new View.OnClickListener() {
             @Override
